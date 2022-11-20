bc1645526bd985bfc522b38cbd1fec158161ff51
Thblslvr <popachubby@mail.ru>
2022-11-20 18:17:34 +0500

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
 
 function App() {
+  const [taskToAdd, setTaskToAdd] = React.useState();
+  const [tasks, setTasks] = React.useState([{ content: '1', inEdit: false }, { content: '2', inEdit: false }]);
+
+  const startEditing = (id, task) => {
+    return (
+      <div key={id} >
+        <input type="text"
+          defaultValue={task.content}
+          onChange={(e) => {
+            const find = tasks.slice();
+            find[id].content = e.target.value;
+            setTasks(find)
+          }
+          }
+        />
+        <button onClick={() => {
+          const find = tasks.slice();
+          find[id].inEdit = false;
+          setTasks(find);
+        }}>save</button>
+      </div>
+    )
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +33,23 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+        <input onChange={(e) => { setTaskToAdd(e.target.value) }} />
+        <button onClick={() => { setTasks(tasks.concat({ content: taskToAdd, inEdit: false })) }}>Add</button>
+
+        {tasks.map((task, id) => {
+          return task.inEdit ? startEditing(id, task) : <div key={id} >{`${id + 1 + ') '}` + task.content}
+            <button onClick={() => { setTasks(tasks.slice().filter((el, i, arr) => el !== arr[id])) }}>Del</button>
+            <button onClick={() => { const find = tasks.slice(); find[id].inEdit = true; setTasks(find); }} >Edit</button>
+          </div>
+        })}
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
